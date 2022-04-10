22f735612cb8e9550b1dbf42947fa12a2fded21c
AhmedDridi-code <ahmed__dridi@outlook.com>
2022-04-10 06:43:44 +0100

    fixing color and form validation

--- src/app/components/products/physical/product-list/product-list.component.ts
+++ src/app/components/products/physical/product-list/product-list.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5c9e4b62ff962c62173b6b005c0cf80009986b28b4a3e263aed5991582f35e1
-size 1416
+oid sha256:792af751e0fba5441b6a4cfcf1a9b3ac31c30bf5f9343483d0fee135b808c9de
+size 1482
--- src/app/shared/service/product.service.ts
+++ src/app/shared/service/product.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f81519608d25e944c3cb5277afa92ae32cdfb4d311da026bd9d1926b9c59b6c7
-size 1491
+oid sha256:581a44be8ce1d3ba4f3d3798f2d4d0c3bb744b1ec7a1fa5b6aa49f9ab4a008cb
+size 1505
