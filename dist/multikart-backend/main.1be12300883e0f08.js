bc6924c4956972714c1b557e2080fa49a5593c43
AhmedDridi-code <ahmed__dridi@outlook.com>
2022-04-10 06:25:57 +0100

    fixing color and form validation

---
+++ dist/multikart-backend/main.1be12300883e0f08.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c0d38a49bf4a5db4f6617520be962990f853f349d8874ddf7975be404c52d20c
+size 2465385
--- dist/multikart-backend/main.689eb9b2a5902584.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c288f42cd94e92dec0158ce48926839cdc48698b7d0d271bf15973b28345cac7
-size 2465381
